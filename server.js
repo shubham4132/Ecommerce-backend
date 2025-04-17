dfaec8f4c6df96b81eebf4a738fb899c512747d2
shubham4132 <shubhamg3122134@gmail.com>
2025-04-17 16:02:30 +0530

    Implemented complete backend flow for shop product listing (model, controller, route)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
+const shopProductsRouter = require("./routes/shop/products-routes");
 //connect to the database
 mongoose
   .connect(
@@ -32,6 +33,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/shop/products", shopProductsRouter);
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
 });
