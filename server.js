b3009676fbde2e17cf676aed0765b10d1a9e3ee0
shubham4132 <shubhamg3122134@gmail.com>
2025-05-14 17:04:20 +0530

    implemented cart add update delete functionality in the cart-controller

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
 const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+
 //connect to the database
 mongoose
   .connect(
@@ -34,6 +36,8 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
+app.use("/api/shop/cart", shopCartRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
 });
